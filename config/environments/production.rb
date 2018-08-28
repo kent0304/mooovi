cd04350390a5866edcd8f359ce773753dc3b3734
Kento Tanaka <>
2018-08-28 21:10:36 +0900

    fix gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,14 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   storage: :s3,
+  bucket: Rails.application.secrets.S3_BUCKET_NAME,
+  s3_region: Rails.application.secrets.AWS_REGION,
+  s3_host_name: 's3-ap-northeast-1.amazonaws.com',
+
+
   s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+    access_key_id: Rails.application.secrets.AWS_ACCESS_KEY_ID
+    secret_access_key: Rails.application.secrets.AWS_SECRET_ACCESS_KEY
   }
 }
 end
